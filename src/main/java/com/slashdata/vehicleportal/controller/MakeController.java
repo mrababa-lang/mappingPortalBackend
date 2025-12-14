e19d2d405ca94b44890b9c490e7fd883a9b9a8ee
mrababa-lang <mrababa@first.tech>
2025-12-14 19:42:11 +0400

    Allow unauthenticated access to models and bulk makes

--- src/main/java/com/slashdata/vehicleportal/controller/MakeController.java
+++ src/main/java/com/slashdata/vehicleportal/controller/MakeController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/makes")
-@PreAuthorize("isAuthenticated()")
 public class MakeController {
 
     private final MakeService makeService;
